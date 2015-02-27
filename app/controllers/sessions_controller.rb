5e918e89c1d8828ef1b6c00836ca265be1250bbc
smshin7 <scottshin1@gmail.com>
2015-02-27 15:07:19 -0800

    changed routes.rb file added omniauth to user model

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,9 +3,11 @@ class SessionsController < ApplicationController
   def create
     user = User.from_omniauth(env["omniauth.auth"])
     session[:user_id] = user["id"]
-    redirect_to user_path(current_user)
+    redirect_to root_path
   end
 
   def destroy 
+    session[:user_id] = nil
+    redirect_to root_path
   end
 end
