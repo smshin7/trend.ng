f011d4d5ec5bee8459f6b7f7de63e84e9df06129
smshin7 <scottshin1@gmail.com>
2015-03-04 19:03:22 -0800

    added scroll to top button on about page

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -21,4 +21,19 @@ $(window).load(function(){
         $scroll.css({ backgroundPosition: coords });    
       }); // end window scroll
    });  // end section function
+
+    //Check to see if the window is top if not then display button
+    $(window).scroll(function(){
+      if ($(this).scrollTop() > 100) {
+        $('.scrollToTop').fadeIn();
+      } else {
+        $('.scrollToTop').fadeOut();
+      }
+    });
+    
+    //Click event to scroll to top
+    $('.scrollToTop').click(function(){
+      $('html, body').animate({scrollTop : 0},800);
+      return false;
+    }); 
 }); // close out script
